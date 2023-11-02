86329f008e5ca2f2d0b5e62853ec347ffb73c4f4
Clark, Kassidy <kassidy.clark@tektronix.com>
2023-11-02 08:39:07 -0700

    adding tool to interact with AD

---
+++ lookupUser.ps1
@@ -0,0 +1,9 @@
+﻿# Given an email adddress, look up the user name in AD.
+
+
+foreach($line in Get-Content $args[0]) {
+    if($line -match $regex){
+        $name=$(Get-ADUser -filter {Emailaddress -eq $line} | Select-Object -ExpandProperty Name)
+        Write-Output "$line $name"
+    }
+}
